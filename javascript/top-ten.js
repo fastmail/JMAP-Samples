0661dd2bf48b17b45478ab6b5ac625a543ec22e6
Joe Woods <woods@fastmailteam.com>
2019-10-23 12:50:08 -0400

    request inbox

--- javascript/top-ten.js
+++ javascript/top-ten.js
@@ -6,7 +6,7 @@ const fetch = require("node-fetch");
 if (!process.env.JMAP_USERNAME || !process.env.JMAP_PASSWORD) {
   console.log("Please set your JMAP_USERNAME and JMAP_PASSWORD");
   console.log(
-    "JMAP_USERNAME=username JMAP_PASSWORD=password node hello-world.js"
+    "JMAP_USERNAME=username JMAP_PASSWORD=password node top-ten.js"
   );
 
   process.exit(1);
@@ -30,7 +30,41 @@ const getSession = async () => {
   return response.json();
 };
 
-const mailboxQuery = async (api_url, account_id) => {
+const inboxIdQuery = async (api_url, account_id) => {
+  const response = await fetch(api_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `basic ${auth_token}`
+    },
+    body: JSON.stringify({
+      using: ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+      methodCalls: [
+        [
+          "Mailbox/query",
+          {
+            accountId: account_id,
+            filter: { role: "inbox", "hasAnyRole": true },
+          },
+          "a"
+        ],
+      ]
+    })
+  });
+
+  const data = await response.json();
+
+  inbox_id = data["methodResponses"][0][1]["ids"][0];
+
+  if (!inbox_id.length) {
+    console.error("Could not get an inbox.");
+    process.exit(1);
+  }
+
+  return await inbox_id;
+};
+
+const mailboxQuery = async (api_url, account_id, inbox_id) => {
   const response = await fetch(api_url, {
     method: "POST",
     headers: {
@@ -44,6 +78,7 @@ const mailboxQuery = async (api_url, account_id) => {
           "Email/query",
           {
             accountId: account_id,
+            filter: { inMailbox: inbox_id },
             sort: [{ property: "receivedAt", isAscending: false }],
             limit: 10
           },
@@ -74,9 +109,11 @@ const mailboxQuery = async (api_url, account_id) => {
 getSession().then(session => {
   const api_url = session.apiUrl;
   const account_id = session.primaryAccounts["urn:ietf:params:jmap:mail"];
-  mailboxQuery(api_url, account_id).then(emails => {
-    emails["methodResponses"][1][1]["list"].forEach(email => {
-      console.log(`${email.receivedAt} — ${email.subject}`);
+  inboxIdQuery(api_url, account_id).then(inbox_id => {
+    mailboxQuery(api_url, account_id, inbox_id).then(emails => {
+      emails["methodResponses"][1][1]["list"].forEach(email => {
+        console.log(`${email.receivedAt} — ${email.subject}`);
+      });
     });
-  });
+  })
 });
