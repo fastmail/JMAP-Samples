fdb28e100325929b54a5a74fb5c957d66e13b2d3
Michael McClimon <michael@mcclimon.org>
2019-10-23 11:14:27 -0400

    python3/top-ten: only get inbox mail

--- python3/top-ten.py
+++ python3/top-ten.py
@@ -5,11 +5,29 @@ import os
 from tiny_jmap_library import TinyJMAPClient
 
 client = TinyJMAPClient(
-    hostname=os.environ.get("JMAP_HOSTNAME", 'betajmap.fastmail.com'),
+    hostname=os.environ.get("JMAP_HOSTNAME", "betajmap.fastmail.com"),
     username=os.environ.get("JMAP_USERNAME"),
     password=os.environ.get("JMAP_PASSWORD"),
 )
 account_id = client.get_account_id()
+
+inbox_res = client.make_jmap_call(
+    {
+        "using": ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+        "methodCalls": [
+            [
+                "Mailbox/query",
+                {
+                    "accountId": account_id,
+                    "filter": {"role": "inbox", "hasAnyRole": True},
+                },
+                "a",
+            ]
+        ],
+    }
+)
+inbox_id = inbox_res["methodResponses"][0][1]["ids"][0]
+assert len(inbox_id) > 0
 
 get_res = client.make_jmap_call(
     {
@@ -19,6 +37,7 @@ get_res = client.make_jmap_call(
                 "Email/query",
                 {
                     "accountId": account_id,
+                    "filter": {"inMailbox": inbox_id},
                     "sort": [{"property": "receivedAt", "isAscending": False}],
                     "limit": 10,
                 },
